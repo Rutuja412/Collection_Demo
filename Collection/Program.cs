e6a44747a463d065e9f171d8e48577150770d1bf
Rutuja412 <rutujakadam4545@gmail.com>
2023-02-01 11:27:42 +0530

    [Rutuja]:Adding Collection

---
+++ Collection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Collection----");
+            
+            Demo.doListDemo();
+            Demo.doStackDemo();
+             Demo.doDictionaryDemo();
+            Demo.doQueueDemo();
+            Demo.doSetDemo();
+            Console.ReadKey();
+        }
+    }
+}
